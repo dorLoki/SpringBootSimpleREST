ba3f6cec0fe5e521918b02dbbb9407fb424e8890
lukeh <lukeh@WIN11>
2024-04-10 17:32:35 +0200

    init

---
+++ AlexaTest/src/main/java/net/heydel/AlexaTestApplication.java
@@ -0,0 +1,13 @@
+package net.heydel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexaTestApplication.class, args);
+	}
+
+}
